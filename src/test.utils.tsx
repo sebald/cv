710a5c029091271c2c61ab9d468a9ffd6eba3f4a
Sebastian Sebald <sebastian.sebald@gmail.com>
2018-03-15 22:43:55 +0100

    💄 Use theme as fallback for `shallowWithTheme`

--- src/test.utils.tsx
+++ src/test.utils.tsx
@@ -3,7 +3,12 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import { ThemeProvider } from 'styled-components';
 
-export function shallowWithTheme<P>(node: React.ReactElement<P>, theme: any) {
+import * as style from './style';
+
+export function shallowWithTheme<P>(
+  node: React.ReactElement<P>,
+  theme: any = style.theme
+) {
   const context = (shallow(
     <ThemeProvider theme={theme} />
   ).instance() as any).getChildContext();
