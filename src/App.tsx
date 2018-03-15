f79f1b5c5f0372babbbb06c152730c1d4425d3c4
Sebastian Sebald <sebastian.sebald@gmail.com>
2018-03-15 20:54:48 +0100

    👷‍♂️ Put everything in the app to test look + feel

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import * as React from 'react';
 
 import { BaselineGrid } from './components/Baseline';
 import * as Layout from './components/Layout';
+import * as Typography from './components/Typography';
 
 export const App: React.SFC = () => (
   <Layout.Container>
-    <Layout.Header>Header</Layout.Header>
+    <Layout.Header>
+      <Typography.Display>Sebastian Sebald</Typography.Display>
+    </Layout.Header>
     <Layout.Main>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis est risus,
-      dignissim at nisl id, tristique cursus odio. Vestibulum fermentum est eu
-      nisi congue, vel auctor metus interdum. Aliquam suscipit convallis enim
-      vitae imperdiet. Integer interdum lacus eu eros fringilla, quis rhoncus
-      urna laoreet. Praesent quis pharetra quam. Sed non orci facilisis,
-      condimentum nisi eget, vehicula mauris. Sed vitae enim nec enim viverra
-      mollis sit amet in odio. Ut ac lorem mollis, facilisis purus nec,
-      sollicitudin ligula.
+      <Typography.Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis est risus,
+        dignissim at nisl id, tristique cursus odio. Vestibulum fermentum est eu
+        nisi congue, vel auctor metus interdum. Aliquam suscipit convallis enim
+        vitae imperdiet. Integer interdum lacus eu eros fringilla, quis rhoncus
+        urna laoreet. Praesent quis pharetra quam. Sed non orci facilisis,
+        condimentum nisi eget, vehicula mauris. Sed vitae enim nec enim viverra
+        mollis sit amet in odio. Ut ac lorem mollis, facilisis purus nec,
+        sollicitudin ligula.
+      </Typography.Text>
+      <Typography.Heading>Heading</Typography.Heading>
+      <Typography.Subheading>Subheading</Typography.Subheading>
+      <Typography.Caption>Caption</Typography.Caption>
     </Layout.Main>
     <Layout.Footer>Footer</Layout.Footer>
     <BaselineGrid />
